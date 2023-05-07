83b6ffd5a88f5231df0d5737762f497490e9fdc3
villirion <75848800+villirion@users.noreply.github.com>
2023-05-07 16:08:32 +0200

    Add files via upload

---
+++ fleur/connexion/session.php
@@ -0,0 +1,35 @@
+<?php
+   session_start();
+   if($_SESSION["autoriser"]!="oui"){
+      header("location:login.php");
+      exit();
+   }
+    $bienvenue="Bonjour et bienvenue ".
+    $_SESSION["prenomNom"].
+    " dans votre espace personnel";
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <style>
+         *{
+            font-family:arial;
+         }
+         body{
+            margin:20px;
+         }
+         a{
+            color:#EE6600;
+            text-decoration:none;
+         }
+         a:hover{
+            text-decoration:underline;
+         }
+      </style>
+   </head>
+   <body onLoad="document.fo.login.focus()">
+      <h2><?php echo $bienvenue?></h2>
+      [ <a href="deconnexion.php">Se déconnecter</a> ]
+   </body>
+</html>
