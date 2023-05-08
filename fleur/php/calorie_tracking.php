e12425bc5b2ca61e9ff1b51c64badd48bcb0d3f7
tarenissav <132828670+tarenissav@users.noreply.github.com>
2023-05-08 14:11:36 +0200

    Add files via upload

--- fleur/php/calorie_tracking.php
+++ fleur/php/calorie_tracking.php
@@ -1,5 +1,6 @@
 <?php
-$sel=$pdo->prepare("select date, food_calories from repas where user_id=?");
+$_SESSION["id"] = 1;
+$sel=$pdo->prepare("select date, food_calorie from repas where user_id=?");
 $sel->execute(array($_SESSION["id"]));
 $tab=$sel->fetchAll();
 
@@ -8,7 +9,7 @@ $calories = array();
 if (!empty($tab)) {
   foreach ($tab as $row) {
     $dates[] = $row['date'];
-    $calories[] = $row['food_calories'];
+    $calories[] = $row['food_calorie'];
   }
 }
 
@@ -28,23 +29,25 @@ $options = array(
 );
 
 // Construire le graphique
-echo "<canvas id='calories-graph' width='1333' height='250'></canvas>
-  <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
-  <script>
-    var ctx = document.getElementById('calories-graph').getContext('2d');
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: " . json_encode($dates) . ",
-        datasets: [{
-          label: 'Calories consommées',
-          data: " . json_encode($calories) . ",
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: " . json_encode($options) . "
-    });
-  </script>";
+echo "<div class='graph-container'>
+        <canvas id='calories-graph' width='1333' height='250'></canvas>
+      </div>
+      <script src='https://cdn.jsdelivr.net/npm/chart.js'></script>
+      <script>
+        var ctx = document.getElementById('calories-graph').getContext('2d');
+        var myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: " . json_encode($dates) . ",
+            datasets: [{
+              label: 'Calories consommées',
+              data: " . json_encode($calories) . ",
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1
+            }]
+          },
+          options: " . json_encode($options) . "
+        });
+      </script>";
 ?>
