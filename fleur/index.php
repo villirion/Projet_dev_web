e15ad4e123cf1a645270605f1039ed220b04d59f
villirion <75848800+villirion@users.noreply.github.com>
2023-05-09 00:44:30 +0200

    Delete fleur directory

--- fleur/index.php
+++
@@ -1,8 +0,0 @@
-<?php include('php/function.php'); ?>
-<?php include('php/navbar.html'); ?>
-<?php include('php/' . $_SESSION['status'] . '.html'); ?>
-</div>
-    <div class="content">
-        <?php include('php/'.$_SESSION['content']); ?>
-    </div>
-<?php include('php/footer.html'); ?>
